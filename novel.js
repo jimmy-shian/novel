4228a7eb1fb18d5313ad6cdcb45b43079084bb6c
jimmy <c109154330@nkust.edu.tw>
2023-02-13 12:33:09 +0800

    load_fileOK

--- novel.js
+++ novel.js
@@ -1,4 +1,30 @@
 window.onload = function() {
+const contentDiv = document.getElementById("content-nav");
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jimmy-shian.github.io/novel/nav.txt", true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE || xhr.status === 200) {
+    contentDiv.innerHTML = xhr.responseText;
+    }
+};
+xhr.send();
+
+const contentEnd = document.getElementById("content-end");
+
+const end = new XMLHttpRequest();
+end.open("GET", "https://jimmy-shian.github.io/novel/end.txt", true);
+end.onreadystatechange = function() {
+    if (end.readyState === XMLHttpRequest.DONE || end.status === 200) {
+    contentEnd.innerHTML = end.responseText;
+    }
+};
+end.send();
+console.log(contentDiv);
+console.log(contentEnd);
+
+
+
     // 翻譯index
 const cardText = document.querySelector('#cardText');
 const changeBtn = document.querySelector('#changeBtn');
@@ -45,29 +71,8 @@ let isChanged = '0';
         isChanged = '0';
         }
     });
-    const contentDiv = document.getElementById("content-nav");
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "nav.txt", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        contentDiv.innerHTML = xhr.responseText;
-      }
-    };
-    xhr.send();
 
-    const contentEnd = document.getElementById("content-end");
-    
-    const end = new XMLHttpRequest();
-    end.open("GET", "nav.txt", true);
-    end.onreadystatechange = function() {
-      if (end.readyState === XMLHttpRequest.DONE && end.status === 200) {
-        contentEnd.innerHTML = end.responseText;
-      }
-    };
-    end.send();
 };
-
 // window.onload = function() {
 
 //     };
