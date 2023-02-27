5056d68ff759d164828e995e2ffd7fdd79b73716
jimmy <c109154330@nkust.edu.tw>
2023-02-28 01:03:46 +0800

    js2

--- index.waku.js
+++ index.waku.js
@@ -1,6 +1,6 @@
    // 翻譯index
-   const cardText = document.querySelector('#cardText');
-   const changeBtn = document.querySelector('#changeBtn');
+    const cardText = document.querySelector('#cardText');
+    const changeBtn = document.querySelector('#changeBtn');
    
    console.log(changeBtn);
    console.log(cardText);
@@ -95,6 +95,8 @@ cardText.innerHTML = `
                `;
 
 window.onload = function() {
+
+};
 const contentDiv = document.getElementById("content-nav");
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://jimmy-shian.github.io/novel/nav.txt", true);
@@ -140,44 +142,42 @@ document.querySelector('#numberInput').addEventListener('keyup', function(event)
         form.submit();
     }
 });
-};
-
 
 const openBtn = document.querySelector('#openBtn');
 const tablelist = document.querySelector('#table-list');
 const loadingIndicator = document.querySelector('#loading-indicator');
 let isOpen = '0';
 
-    openBtn.addEventListener('click', function() {
-    if (isOpen === '0') {
-    // 显示加载指示器
-    loadingIndicator.style.display = 'block';
+openBtn.addEventListener('click', function() {
+if (isOpen === '0') {
+// 显示加载指示器
+loadingIndicator.style.display = 'block';
 
-    const table = new XMLHttpRequest();
-    table.open('GET', 'table.txt', true);
-    table.onreadystatechange = function() {
-        if (table.readyState === XMLHttpRequest.DONE) {
-        // 隐藏加载指示器
-        loadingIndicator.style.display = 'none';
+const table = new XMLHttpRequest();
+table.open('GET', 'table.txt', true);
+table.onreadystatechange = function() {
+    if (table.readyState === XMLHttpRequest.DONE) {
+    // 隐藏加载指示器
+    loadingIndicator.style.display = 'none';
 
-        if (table.status === 200) {
-            tablelist.innerHTML = table.responseText;
-            openBtn.innerHTML = `收起章節列表
-            `;
-            isOpen = '1';
-        } else {
-            tablelist.innerHTML = '加載數據失敗，请稍后重试。';
-        }
-        }
-    };
-    table.send();
+    if (table.status === 200) {
+        tablelist.innerHTML = table.responseText;
+        openBtn.innerHTML = `收起章節列表
+        `;
+        isOpen = '1';
     } else {
-    openBtn.innerHTML = `展開章節列表
-    `;
-    tablelist.innerHTML = `
-    `;
-    isOpen = '0';
+        tablelist.innerHTML = '加載數據失敗，请稍后重试。';
     }
+    }
+};
+table.send();
+} else {
+openBtn.innerHTML = `展開章節列表
+`;
+tablelist.innerHTML = `
+`;
+isOpen = '0';
+}
 
-    });
+});
                 
