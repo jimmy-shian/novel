46f2eb882437102159b3aa9b2f88e2c5f34fbd54
洗柏詞 <c109154330@nkust.edu.tw>
2023-02-28 00:50:31 +0800

    js2

--- index.waku.js
+++ index.waku.js
@@ -1,1 +1,183 @@
-(()=>{const e=document.querySelector("#cardText"),n=document.querySelector("#changeBtn");console.log(n),console.log(e);let t="0";n.addEventListener("click",(function(){"0"===t?(n.innerHTML="翻譯成英文\n        ",e.innerHTML='\n        <br>\n        <h4 class="card-title">進入網絡小說世界~</h4>\n        \n        看啊，一個新的平台到來了，<br>\n        虛構的故事正在努力，<br>\n        娛樂和迷住思想，<br>\n        在文字的世界中，找到一種快樂。<br>\n        <br>\n        無廣告，內容來源精心，<br>\n        由學生使用技術公平，<br>\n        網絡小說的世界得到修復，<br>\n        並提供難得的閱讀體驗。<br>\n        <br>\n        親愛的讀者，來吧，坐下來，<br>\n        喝一杯你最喜歡的飲料，<br>\n        讓自己沉浸在這個如此甜蜜的世界<br>\n        裡，冒險和愛情的故事在這里相遇。<br>\n        <br>\n        享受你的閱讀吧，在這個明亮的地方，讓<br>\n        自己沉浸在無盡黑夜的故事中。\n    <div class="spacer"></div>\n\n        ',t="1"):(n.innerHTML="Translate to Chinese\n        ",e.innerHTML='\n            <h5 class="card-title">Enter the World of Web Novels</h5>\n                <p class="card-text">\n                Behold, a new platform doth arrive,<br>\n                With tales of fiction that doth strive,<br>\n                To entertain and captivate the mind,<br>\n                In a world of letters, a joy to find.\n                </p>\n                <p class="card-text">\n            Ad-free, with content sourced with care,<br>\n            By a student using technology fair,<br>\n            The world of web novels doth repair,<br>\n            And offer a reading experience rare.\n                </p>\n                <p class="card-text">\n            So come, dear reader, and take a seat,<br>\n            With a cup of thy favored drink to heat,<br>\n            And immerse thyself in this world so sweet,<br>\n            Where tales of adventure and love doth meet.\n                </p>\n                <p class="card-text">\n            Enjoy thy reading, in this place so bright,<br>\n            And lose thyself in tales of endless night.\n                </p>\n                ',t="0")})),window.onload=function(){e.innerHTML='\n        <h5 class="card-title">Enter the World of Web Novels</h5>\n            <p class="card-text">\n            Behold, a new platform doth arrive,<br>\n            With tales of fiction that doth strive,<br>\n            To entertain and captivate the mind,<br>\n            In a world of letters, a joy to find.\n            </p>\n            <p class="card-text">\n        Ad-free, with content sourced with care,<br>\n        By a student using technology fair,<br>\n        The world of web novels doth repair,<br>\n        And offer a reading experience rare.\n            </p>\n            <p class="card-text">\n        So come, dear reader, and take a seat,<br>\n        With a cup of thy favored drink to heat,<br>\n        And immerse thyself in this world so sweet,<br>\n        Where tales of adventure and love doth meet.\n            </p>\n            <p class="card-text">\n        Enjoy thy reading, in this place so bright,<br>\n        And lose thyself in tales of endless night.\n            </p>\n            '}})(),window.onload=function(){const e=document.getElementById("content-nav"),n=new XMLHttpRequest;n.open("GET","https://jimmy-shian.github.io/novel/nav.txt",!0),n.onreadystatechange=function(){n.readyState!==XMLHttpRequest.DONE&&200!==n.status||(e.innerHTML=n.responseText)},n.send();const t=document.getElementById("content-end"),r=new XMLHttpRequest;r.open("GET","https://jimmy-shian.github.io/novel/end.txt",!0),r.onreadystatechange=function(){r.readyState!==XMLHttpRequest.DONE&&200!==r.status||(t.innerHTML=r.responseText)},r.send(),console.log(e),console.log(t);const o=document.querySelector("h2").textContent.trim().match(/《([^》]*)》/),a=o?o[1]:"";console.log(a);const s=document.querySelector("form");s.addEventListener("submit",(function(e){e.preventDefault();const n=document.querySelector("#numberInput").value.trim();/^\d+$/.test(n)?window.location.href=`${a}_html${n}.html`:alert("輸入數字")})),document.querySelector("#numberInput").addEventListener("keyup",(function(e){13===e.keyCode&&s.submit()}))},(()=>{const e=document.querySelector("#openBtn"),n=document.querySelector("#table-list"),t=document.querySelector("#loading-indicator");let r="0";e.addEventListener("click",(function(){if("0"===r){t.style.display="block";const o=new XMLHttpRequest;o.open("GET","table.txt",!0),o.onreadystatechange=function(){o.readyState===XMLHttpRequest.DONE&&(t.style.display="none",200===o.status?(n.innerHTML=o.responseText,e.innerHTML="收起章節列表\n            ",r="1"):n.innerHTML="加載數據失敗，请稍后重试。")},o.send()}else e.innerHTML="展開章節列表\n    ",n.innerHTML="\n    ",r="0"}))})();
+   // 翻譯index
+   const cardText = document.querySelector('#cardText');
+   const changeBtn = document.querySelector('#changeBtn');
+   
+   console.log(changeBtn);
+   console.log(cardText);
+   
+   let isChanged = '0';
+           
+       changeBtn.addEventListener('click', function changeindextex() {
+           if (isChanged === '0') {
+           changeBtn.innerHTML = `翻譯成英文
+           `;
+           cardText.innerHTML = `
+           <h4 class="card-title">進入網絡小說世界~</h4>
+           
+           看啊，一個新的平台到來了，<br>
+           虛構的故事正在努力，<br>
+           娛樂和迷住思想，<br>
+           在文字的世界中，找到一種快樂。<br>
+           <br>
+           無廣告，內容來源精心，<br>
+           由學生使用技術公平，<br>
+           網絡小說的世界得到修復，<br>
+           並提供難得的閱讀體驗。<br>
+           <br>
+           親愛的讀者，來吧，坐下來，<br>
+           喝一杯你最喜歡的飲料，<br>
+           讓自己沉浸在這個如此甜蜜的世界<br>
+           裡，冒險和愛情的故事在這里相遇。<br>
+           <br>
+           享受你的閱讀吧，在這個明亮的地方，讓<br>
+           自己沉浸在無盡黑夜的故事中。
+       <div class="spacer"></div>
+   
+           `;
+           isChanged = '1';
+   
+           } else {
+           changeBtn.innerHTML = `Translate to Chinese
+           `;
+           cardText.innerHTML = `
+               <h5 class="card-title">Enter the World of Web Novels</h5>
+                   <p class="card-text">
+                   Behold, a new platform doth arrive,<br>
+                   With tales of fiction that doth strive,<br>
+                   To entertain and captivate the mind,<br>
+                   In a world of letters, a joy to find.
+                   </p>
+                   <p class="card-text">
+               Ad-free, with content sourced with care,<br>
+               By a student using technology fair,<br>
+               The world of web novels doth repair,<br>
+               And offer a reading experience rare.
+                   </p>
+                   <p class="card-text">
+               So come, dear reader, and take a seat,<br>
+               With a cup of thy favored drink to heat,<br>
+               And immerse thyself in this world so sweet,<br>
+               Where tales of adventure and love doth meet.
+                   </p>
+                   <p class="card-text">
+               Enjoy thy reading, in this place so bright,<br>
+               And lose thyself in tales of endless night.
+                   </p>
+                   `;
+           isChanged = '0';
+           }
+       });
+   
+cardText.innerHTML = `
+           <h5 class="card-title">Enter the World of Web Novels</h5>
+               <p class="card-text">
+               Behold, a new platform doth arrive,<br>
+               With tales of fiction that doth strive,<br>
+               To entertain and captivate the mind,<br>
+               In a world of letters, a joy to find.
+               </p>
+               <p class="card-text">
+           Ad-free, with content sourced with care,<br>
+           By a student using technology fair,<br>
+           The world of web novels doth repair,<br>
+           And offer a reading experience rare.
+               </p>
+               <p class="card-text">
+           So come, dear reader, and take a seat,<br>
+           With a cup of thy favored drink to heat,<br>
+           And immerse thyself in this world so sweet,<br>
+           Where tales of adventure and love doth meet.
+               </p>
+               <p class="card-text">
+           Enjoy thy reading, in this place so bright,<br>
+           And lose thyself in tales of endless night.
+               </p>
+               `;
+
+window.onload = function() {
+const contentDiv = document.getElementById("content-nav");
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jimmy-shian.github.io/novel/nav.txt", true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE || xhr.status === 200) {
+    contentDiv.innerHTML = xhr.responseText;
+    }
+};
+xhr.send();
+
+const contentEnd = document.getElementById("content-end");
+
+const end = new XMLHttpRequest();
+end.open("GET", "https://jimmy-shian.github.io/novel/end.txt", true);
+end.onreadystatechange = function() {
+    if (end.readyState === XMLHttpRequest.DONE || end.status === 200) {
+    contentEnd.innerHTML = end.responseText;
+    }
+};
+end.send();
+console.log(contentDiv);
+console.log(contentEnd);
+
+
+const title = document.querySelector('h2').textContent.trim();
+const match = title.match(/《([^》]*)》/);
+const bookTitle = match ? match[1] : '';
+console.log(bookTitle);
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const numberInput = document.querySelector('#numberInput').value.trim();
+    if (!/^\d+$/.test(numberInput)) {
+        alert('輸入數字');
+    } else {
+        window.location.href = `${bookTitle}_html${numberInput}.html`;
+    }
+});
+
+document.querySelector('#numberInput').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        form.submit();
+    }
+});
+};
+
+
+const openBtn = document.querySelector('#openBtn');
+const tablelist = document.querySelector('#table-list');
+const loadingIndicator = document.querySelector('#loading-indicator');
+let isOpen = '0';
+
+    openBtn.addEventListener('click', function() {
+    if (isOpen === '0') {
+    // 显示加载指示器
+    loadingIndicator.style.display = 'block';
+
+    const table = new XMLHttpRequest();
+    table.open('GET', 'table.txt', true);
+    table.onreadystatechange = function() {
+        if (table.readyState === XMLHttpRequest.DONE) {
+        // 隐藏加载指示器
+        loadingIndicator.style.display = 'none';
+
+        if (table.status === 200) {
+            tablelist.innerHTML = table.responseText;
+            openBtn.innerHTML = `收起章節列表
+            `;
+            isOpen = '1';
+        } else {
+            tablelist.innerHTML = '加載數據失敗，请稍后重试。';
+        }
+        }
+    };
+    table.send();
+    } else {
+    openBtn.innerHTML = `展開章節列表
+    `;
+    tablelist.innerHTML = `
+    `;
+    isOpen = '0';
+    }
+
+    });
+                
